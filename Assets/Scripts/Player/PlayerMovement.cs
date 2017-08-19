a28b3bf3c100be574141cded121eaa2fab81f8a8
Vivek Hari <vivek.hari@outlook.co.nz>
2017-08-19 19:00:21 +1000

    Move player using WASD keys.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed = 4.0f;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        // move player based on current speed
+        Vector3 horizontalDir = Camera.main.transform.right;
+        Vector3 verticalDir = Vector3.Cross(Vector3.down, Camera.main.transform.right);
+        Vector3 movement = (horizontalDir * Input.GetAxis("Horizontal"))
+            + (verticalDir * Input.GetAxis("Vertical"));
+        transform.Translate(movement * MovementSpeed, Space.World);
+    }
+}
