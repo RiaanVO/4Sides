c5f91c0c46eb536e3de5a5a4764305b9f9f57e81
Vivek Hari <vivek.hari@outlook.co.nz>
2017-08-19 19:08:04 +1000

    Make camera follow the player.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;            // The position that that camera will be following.
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+
+    Vector3 offset;                     // The initial offset from the target.
+
+    void Start()
+    {
+        if (target == null)
+            return;
+
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+            return;
+
+        // Create a position the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(
+            transform.position, targetCamPos,
+            smoothing * Time.deltaTime);
+    }
+}
