ec7fd297fcb342cf73c0e8a0d1e3df1f0da500df
mejiaken19 <mejiaken19>
2017-08-19 22:37:14 +1000

    added enemy spawn and seeker

---
+++ Assets/Scripts/SpawnPoint/SpawnerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnTime;
+	private float timeSinceSpawn;
+
+
+
+
+	void start () {
+		Time.timeScale = 0;
+		timeSinceSpawn = 0;
+		SpawnEnemy();
+
+	}
+
+	void Update () {
+		timeSinceSpawn += Time.deltaTime;
+
+		if (timeSinceSpawn >= spawnTime) {
+			SpawnEnemy ();
+			timeSinceSpawn = 0;
+		}
+	}
+
+	void SpawnEnemy() 
+	{
+		Instantiate (enemy, transform.position, Quaternion.identity);	
+	}
+}
