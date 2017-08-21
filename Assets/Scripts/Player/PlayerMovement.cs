ba4bf95ea0c1812ba85d131543d795bda73327ef
AnmingLiu <manu_eva@163.com>
2017-08-21 10:25:13 +1000

    add boundary and player rotation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,8 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public float MovementSpeed = 4.0f;
 
+    Rigidbody playerBody;
+    int floorMask;
+    float camRayLength = 100f;
+
     void Start()
     {
+        floorMask = LayerMask.GetMask("Floor");
+        playerBody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
@@ -18,5 +24,21 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = (horizontalDir * Input.GetAxis("Horizontal"))
             + (verticalDir * Input.GetAxis("Vertical"));
         transform.Translate(movement * MovementSpeed, Space.World);
+
+        turning();
+    }
+
+    void turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerBody.MoveRotation(newRotation);
+        }
     }
 }
