3f70299fe3529937eb774f197ed943ce1aec1341
Vivek Hari <vivek.hari@outlook.co.nz>
2017-08-23 21:23:55 +1000

    Fix bug where aiming did not work when cursor was outside of room.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,39 +6,30 @@ public class PlayerMovement : MonoBehaviour
 {
     public float MovementSpeed = 4.0f;
 
-    Rigidbody playerBody;
-    int floorMask;
-    float camRayLength = 100f;
-
-    void Start()
-    {
-        floorMask = LayerMask.GetMask("Floor");
-        playerBody = GetComponent<Rigidbody>();
-    }
-
     void FixedUpdate()
     {
+        // calculate world position of mouse cursor
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var plane = new Plane(Vector3.up, Vector3.zero);
+        float distance = 0.0f;
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 mousePos = ray.GetPoint(distance);
+
+            // calculate rotation based on direction to mouse cursor
+            Vector3 dirToMouse = mousePos - transform.position;
+            float rotation = -Mathf.Atan2(dirToMouse.z, dirToMouse.x)
+                * Mathf.Rad2Deg;
+
+            // set the player rotation
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
         // move player based on current speed
         Vector3 horizontalDir = Camera.main.transform.right;
         Vector3 verticalDir = Vector3.Cross(Vector3.down, Camera.main.transform.right);
         Vector3 movement = (horizontalDir * Input.GetAxis("Horizontal"))
             + (verticalDir * Input.GetAxis("Vertical"));
         transform.Translate(movement * MovementSpeed, Space.World);
-
-        turning();
-    }
-
-    void turning()
-    {
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit floorHit;
-        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
-        {
-            Vector3 playerToMouse = floorHit.point - transform.position;
-            playerToMouse.y = 0f;
-
-            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-            playerBody.MoveRotation(newRotation);
-        }
     }
 }
