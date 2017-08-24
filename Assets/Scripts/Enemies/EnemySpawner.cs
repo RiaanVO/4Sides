2d2b2d8fd037c03d158f2902891d8ce45cd4009e
Vivek Hari <vivek.hari@outlook.co.nz>
2017-08-24 22:54:08 +1000

    Adjusted spawner appearance.

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -9,6 +9,7 @@ public class EnemySpawner : MonoBehaviour
     public Material IdleMaterial;
     public Material ActiveMaterial;
     public float TelegraphDuration = 1.0f;
+    public Vector3 EnemySpawnOffset;
 
     private Renderer render;
     private bool isSpawning;
@@ -26,22 +27,31 @@ public class EnemySpawner : MonoBehaviour
 
     void Update()
     {
-        if (isSpawning && Time.time - telegraphStartTimestamp > TelegraphDuration)
+        // are we telegraphing a spawn?
+        if (isSpawning)
         {
-            SpawnEnemy();
-            isSpawning = false;
-            render.material = IdleMaterial;
+            if (Time.time - telegraphStartTimestamp > TelegraphDuration)
+            {
+                // spawn the enemy
+                SpawnEnemy();
+                isSpawning = false;
+                render.material = IdleMaterial;
+            }
+            else
+            {
+                // blink to telegraph
+                render.material = Time.time % 0.3f > 0.15f ? IdleMaterial : ActiveMaterial;
+            }
         }
     }
 
     private void SpawnEnemy()
     {
-        Instantiate(Enemy, transform.position, Quaternion.identity);
+        Instantiate(Enemy, transform.position + EnemySpawnOffset, Quaternion.identity);
     }
 
     public void Spawn()
     {
-        render.material = ActiveMaterial;
         isSpawning = true;
         telegraphStartTimestamp = Time.time;
     }
