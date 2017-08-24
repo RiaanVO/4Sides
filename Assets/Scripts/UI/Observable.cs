286ba029a40145e17f7c28091a6fbe305c2abd0b
Vivek Hari <vivek.hari@outlook.co.nz>
2017-08-25 01:46:35 +1000

    Abstracted observer pattern into Observable behaviour.

---
+++ Assets/Scripts/UI/Observable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Observable : MonoBehaviour, IDataObserver
+{
+    public delegate void ChannelListener<T>(T newValue);
+    public DataProvider Provider;
+
+    private Dictionary<string, Action<object>> channelListeners =
+        new Dictionary<string, Action<object>>();
+
+    public void Bind<T>(string channel, T defaultValue, ChannelListener<T> listener)
+    {
+        if (Provider != null)
+        {
+            Provider.SubscribeToChannels(this, channel);
+        }
+
+        channelListeners[channel] = (object data) => listener((T)data);
+        listener(defaultValue);
+
+        Render();
+    }
+
+    public void OnChannelUpdated(string channel, object newValue)
+    {
+        Action<object> listener;
+        if (channelListeners.TryGetValue(channel, out listener))
+        {
+            listener(newValue);
+            Render();
+        }
+    }
+
+    public void OnEventTriggered(string eventName)
+    {
+    }
+
+    protected abstract void Render();
+}
--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -4,51 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
-public class ProgressBar : MonoBehaviour, IDataObserver
+public class ProgressBar : Observable
 {
-    public DataProvider Provider;
     public string ValueChannel;
     public string MaxValueChannel;
     public float DefaultValue = 0.5f;
     public float DefaultMaxValue = 1.0f;
 
     private Image image;
-    private float maxValue;
     private float value;
+    private float maxValue;
 
     void Start()
     {
         image = GetComponent<Image>();
 
-        value = DefaultValue;
-        maxValue = DefaultMaxValue;
-        Render();
-
-        if (Provider != null)
-        {
-            Provider.SubscribeToChannels(this, ValueChannel, MaxValueChannel);
-        }
+        Bind<float>(ValueChannel, DefaultValue, newValue => value = newValue);
+        Bind<float>(MaxValueChannel, DefaultMaxValue, newValue => maxValue = newValue);
     }
 
-    public void OnChannelUpdated(string channel, object newValue)
-    {
-        if (channel == MaxValueChannel && newValue is float)
-        {
-            maxValue = (float)newValue;
-            Render();
-        }
-        else if (channel == ValueChannel && newValue is float)
-        {
-            value = (float)newValue;
-            Render();
-        }
-    }
-
-    public void OnEventTriggered(string eventName)
-    {
-    }
-
-    private void Render()
+    protected override void Render()
     {
         image.fillAmount = value / maxValue;
     }
