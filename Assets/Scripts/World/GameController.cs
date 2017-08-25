bd36d1e98fa07fcb7fcbebcbf4c30a38d8e521e3
Vivek Hari <vivek.hari@outlook.co.nz>
2017-08-25 22:41:40 +1000

    Navigate to death screen after player dies.

---
+++ Assets/Scripts/World/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour, IDataObserver
+{
+    public DataProvider Player;
+
+    void Start()
+    {
+        if (Player != null)
+        {
+            Player.SubscribeToEvents(this, BaseHealth.EVENT_DIED);
+        }
+    }
+
+    public void OnChannelUpdated(string channel, object newValue)
+    {
+    }
+
+    public void OnEventTriggered(string eventName)
+    {
+        SceneManager.LoadScene("DeathScene");
+    }
+}
