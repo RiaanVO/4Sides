c9069966f21d211ab39d9591a97b7700d20a7478
Vivek Hari <vivek.hari@outlook.co.nz>
2017-09-01 18:56:58 +1000

    Create drop pods at spawn locations.

--- Assets/Scripts/Enemies/EnemySpawnManager.cs
+++ Assets/Scripts/Enemies/EnemySpawnManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
@@ -8,8 +9,9 @@ public class EnemySpawnManager : MonoBehaviour
     public float MinimumTimeBetweenSpawns = 0.1f;
     public float TimeBetweenSpawnRateIncreases = 10.0f;
     public float SpawnIntervalDecrease = 0.5f;
+    public GameObject DropPod;
 
-    private EnemySpawner[] spawners;
+    private List<Vector3> spawnPoints;
     private float currentTimeBetweenSpawns;
 
     private float lastSpawnedTimestamp;
@@ -17,15 +19,23 @@ public class EnemySpawnManager : MonoBehaviour
 
     void Start()
     {
-        spawners = Object.FindObjectsOfType<EnemySpawner>();
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint").Select(o => o.transform.position).ToList();
         currentTimeBetweenSpawns = InitialTimeBetweenSpawns;
 
         lastSpawnedTimestamp = Time.time;
         rateLastIncreasedTimestamp = Time.time;
+
+        if (DropPod == null)
+        {
+            Debug.LogError("No drop pod specified!");
+        }
+
+        StartNewWave();
     }
 
     void Update()
     {
+        /*
         if (Time.time - lastSpawnedTimestamp > currentTimeBetweenSpawns)
         {
             // spawn an enemy
@@ -40,14 +50,36 @@ public class EnemySpawnManager : MonoBehaviour
                 MinimumTimeBetweenSpawns);
             rateLastIncreasedTimestamp = Time.time;
         }
+        */
+    }
+
+    private void StartNewWave()
+    {
+        // determine how many drop pods to spawn
+        int dropPodCount = Mathf.Min(5, spawnPoints.Count);
+
+        List<int> possibleSpawnIndices = Enumerable.Range(0, spawnPoints.Count).ToList();
+        List<Vector3> spawnLocations = new List<Vector3>();
+        for (int i = 0; i < dropPodCount; i++)
+        {
+            // pick a random spawn point
+            int index = Random.Range(0, possibleSpawnIndices.Count);
+            spawnLocations.Add(spawnPoints[possibleSpawnIndices[index]]);
+            possibleSpawnIndices.RemoveAt(index);
+        }
+
+        // create drop pods
+        spawnLocations.ForEach(pos => Instantiate(DropPod, pos, Quaternion.identity));
     }
 
     private void Spawn()
     {
-        if (spawners.Length == 0) return;
+        /*
+        if (spawnPoints.Count == 0) return;
 
         // pick a random spawner and tell it to spawn an enemy
         var spawner = spawners[Random.Range(0, spawners.Length)];
         spawner.Spawn();
+        */
     }
 }
