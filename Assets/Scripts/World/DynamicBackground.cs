caca1df6fb21c96a6fa0fa247eb2d2ae46f5d2a7
Vivek Hari <vivek.hari@outlook.co.nz>
2017-09-05 01:58:07 +1000

    Change emission of floor over time.

---
+++ Assets/Scripts/World/DynamicBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicBackground : MonoBehaviour
+{
+    public Renderer TargetRenderer;
+    public Color LowIntensityColor;
+    public Color HighIntensityColor;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float intensity = Mathf.Sin(Time.time);
+        Color color = Color.Lerp(LowIntensityColor, HighIntensityColor, intensity);
+        TargetRenderer.material.SetColor("_EmissionColor", color);
+    }
+}
