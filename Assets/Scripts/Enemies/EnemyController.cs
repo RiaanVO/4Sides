e0d9ec581596625bb451242abd8f96645a60f6db
Riaan Van Onselen <riaanvo@yahoo.com.sg>
2017-09-25 09:40:35 +1000

    Created a simple script for detecting the player and playing a sound once the player has been found. It performs this based on proximity checks with distance

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -15,32 +15,73 @@ public class EnemyController : PooledObject
 	public float positionCheckDelay = 0.5f;
 	private float checkTimer = 0f;
 
+	private bool playerFound = false;
+	public float playerDetectionRadius = 6f;
+
+	public AudioClip playerDetectedSFX;
+	private AudioSource detectedSoundPlayer;
+
+	void Awake(){
+		detectedSoundPlayer = AddAudioSource(playerDetectedSFX, 1.0f);
+	}
+
+	public void Initialize(Vector3 position)
+	{
+		nav = GetComponent<NavMeshAgent>();
+		health = GetComponent<EnemyHealth>();
+		player = Object.FindObjectOfType<PlayerMovement>();
+
+		nav.enabled = false;
+		transform.position = position;
+		nav.enabled = true;
+
+		health.ResetHealth();
+
+		var manager = GameObject.FindObjectOfType<EnemySpawnManager>();
+		manager.RegisterEnemy(this);
+
+
+		//Player detection settings
+		playerFound = false;
+		checkTimer = positionCheckDelay;
+	}
 
     void Update()
     {
         if (player != null)
         {
-			checkTimer += Time.deltaTime;
-			if(checkTimer > positionCheckDelay || checkDistance > Vector3.Distance(transform.position, player.transform.position)){
-				checkTimer = 0f;
-            	nav.SetDestination(player.transform.position);
+			if (playerFound == false) {
+				checkIfPlayerFound ();
+			} else {
+				checkTimer += Time.deltaTime;
+				if (checkTimer > positionCheckDelay || checkDistance > Vector3.Distance (transform.position, player.transform.position)) {
+					checkTimer = 0f;
+					nav.SetDestination (player.transform.position);
+				}
 			}
         }
     }
 
-    public void Initialize(Vector3 position)
-    {
-        nav = GetComponent<NavMeshAgent>();
-        health = GetComponent<EnemyHealth>();
-        player = Object.FindObjectOfType<PlayerMovement>();
+	private void checkIfPlayerFound(){
+		if (playerDetectionRadius > Vector3.Distance (transform.position, player.transform.position)) {
+			notifyPlayerFound ();
+		}
+	}
 
-        nav.enabled = false;
-        transform.position = position;
-        nav.enabled = true;
+	private void notifyPlayerFound(){
+		if (playerFound == false) {
+			detectedSoundPlayer.Play ();
+			playerFound = true;
+		}
+	}
 
-        health.ResetHealth();
-
-        var manager = GameObject.FindObjectOfType<EnemySpawnManager>();
-        manager.RegisterEnemy(this);
-    }
+	private AudioSource AddAudioSource(AudioClip clip, float volume)
+	{
+		var source = gameObject.AddComponent<AudioSource>();
+		source.clip = clip;
+		source.volume = volume;
+		source.playOnAwake = false;
+		source.spatialBlend = 0.75f;
+		return source;
+	}
 }
