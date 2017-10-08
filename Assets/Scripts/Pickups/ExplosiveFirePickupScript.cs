b7bb22cc3974ecac028d06fa0b0f60631ccd6e05
AnmingLiu <manu_eva@163.com>
2017-10-08 11:17:29 +1100

    modify the pickup spawn script so it can spawn both health and ammo, the new script will never crash the game

---
+++ Assets/Scripts/Pickups/ExplosiveFirePickupScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveFirePickupScript : MonoBehaviour {
+
+    private const string PLAYER_TAG = "Player";
+    public float ExplosiveFireRate = 0.2f;
+    public int ExplosiveFireRateDuration = 10;
+
+    public Bullet NewBullet;
+
+    public AudioClip ExplosiveFireSpawnSFX;
+    public AudioClip ExplosiveFireCollectedSFX;
+
+    private Renderer bulletMaterial;
+    private AudioSource audioSource;
+    public GameObject model;
+    public GameObject pickupLight;
+    private bool isCollected = false;
+
+    private PickupAnimation pickupAnimation;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        SetVisibility(true);
+    }
+
+    public void SpawnExplosiveFirePickup(Vector3 newPosition)
+    {
+        if (pickupAnimation == null)
+        {
+            pickupAnimation = GetComponent<PickupAnimation>();
+        }
+
+        pickupAnimation.SetBouncePositions(newPosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y, newPosition.z);
+
+        if (audioSource != null && ExplosiveFireSpawnSFX != null)
+        {
+            audioSource.PlayOneShot(ExplosiveFireSpawnSFX);
+        }
+        SetVisibility(true);
+        isCollected = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(PLAYER_TAG) && !isCollected)
+        {
+            PlayerShooting player = other.gameObject.GetComponentInParent<PlayerShooting>();
+            if (player != null)
+            {
+                isCollected = true;
+                if (audioSource != null && ExplosiveFireCollectedSFX != null)
+                {
+                    audioSource.PlayOneShot(ExplosiveFireCollectedSFX);
+                }
+
+                SetVisibility(false);
+                player.ChangeFireRate(ExplosiveFireRateDuration, ExplosiveFireRate, NewBullet);
+            }
+        }
+    }
+
+    private void SetVisibility(bool active)
+    {
+        model.SetActive(active);
+        pickupLight.SetActive(active);
+    }
+
+    public bool IsCollected()
+    {
+        return isCollected;
+    }
+}
--- Assets/Scripts/Pickups/PickupSpawnManager.cs
+++ Assets/Scripts/Pickups/PickupSpawnManager.cs
@@ -3,37 +3,113 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class PickupSpawnManager : MonoBehaviour {
+public class PickupSpawnManager : MonoBehaviour
+{
 
-	private List<Vector3> healthSpawnPoints;
-	private HealthPickup healthPickup;
-	private RapidFirePickupScript RapidFirePickup;
-	private int RandomPickupToSpawn;
+    private List<Vector3> healthSpawnPoints;
+    private HealthPickup healthPickup;
+    private RapidFirePickupScript RapidFirePickup;
+    private ExplosiveFirePickupScript ExplosiveFirePickup;
+    private int RandomPickupToSpawn;
+    private int CurrentHealthIndex;
+    private int CurrentFireIndex;
 
-	// Use this for initialization
-	void Start () {
-		healthSpawnPoints = GameObject.FindGameObjectsWithTag("HealthSpawnPoint")
-			.Select(o => o.transform.position).ToList();
+    // Use this for initialization
+    void Start()
+    {
+        healthSpawnPoints = GameObject.FindGameObjectsWithTag("HealthSpawnPoint")
+            .Select(o => o.transform.position).ToList();
 
-		healthPickup = GetComponentInChildren<HealthPickup> ();
-		RapidFirePickup = GetComponentInChildren<RapidFirePickupScript> ();
-	}
+        healthPickup = GetComponentInChildren<HealthPickup>();
+        RapidFirePickup = GetComponentInChildren<RapidFirePickupScript>();
+        ExplosiveFirePickup = GetComponentInChildren<ExplosiveFirePickupScript>();
+    }
 
-	public void SpawnPickup(){
-		if (healthPickup == null || RapidFirePickup == null)
-			return;
-		if (healthPickup.IsCollected () || RapidFirePickup.IsCollected()) {
-			int index = Random.Range(0, healthSpawnPoints.Count);
+    public void SpawnPickup()
+    {
+        if (healthSpawnPoints.Count > 1)
+        {
+            if (healthPickup != null)
+            {
+                if (healthPickup.IsCollected())
+                {
+                    int healthIndex = Random.Range(0, (healthSpawnPoints.Count - 1));
+                    while (healthIndex == CurrentFireIndex)
+                    {
+                        healthIndex = Random.Range(0, (healthSpawnPoints.Count - 1));
+                    }
+                    healthPickup.SpawnHealthPickup(healthSpawnPoints.ElementAt(healthIndex));
+                    CurrentHealthIndex = healthIndex;
+                }
+            }
+            if (RapidFirePickup != null && ExplosiveFirePickup != null)
+            {
+                if (RapidFirePickup.IsCollected() || ExplosiveFirePickup.IsCollected())
+                {
+                    int index = Random.Range(0, healthSpawnPoints.Count);
+                    while (index == CurrentHealthIndex)
+                    {
+                        index = Random.Range(0, healthSpawnPoints.Count);
+                    }
 
-			RandomPickupToSpawn = Random.Range (1, 100);
-			if (RandomPickupToSpawn <= 50) {
-				healthPickup.SpawnHealthPickup (healthSpawnPoints.ElementAt (index));
-			}
-			else if (RandomPickupToSpawn >=51) {
-				RapidFirePickup.SpawnRapidFirePickup (healthSpawnPoints.ElementAt (index));
-			}
-		}
-	}
+                    RandomPickupToSpawn = Random.Range(1, 100);
+                    if (RandomPickupToSpawn <= 50)
+                    {
+                        ExplosiveFirePickup.SpawnExplosiveFirePickup(healthSpawnPoints.ElementAt(index));
+                    }
+                    else if (RandomPickupToSpawn >= 51)
+                    {
+                        RapidFirePickup.SpawnRapidFirePickup(healthSpawnPoints.ElementAt(index));
+                    }
+                    CurrentFireIndex = index;
+                }
+            }
+            else if (RapidFirePickup != null)
+            {
+                if (RapidFirePickup.IsCollected())
+                {
+                    int index = Random.Range(0, healthSpawnPoints.Count);
+                    while (index == CurrentHealthIndex)
+                    {
+                        index = Random.Range(0, healthSpawnPoints.Count);
+                    }
+                    RapidFirePickup.SpawnRapidFirePickup(healthSpawnPoints.ElementAt(index));
+                    CurrentFireIndex = index;
+                }
+            }
+            else if (ExplosiveFirePickup != null)
+            {
+                if (ExplosiveFirePickup.IsCollected())
+                {
+                    int index = Random.Range(0, healthSpawnPoints.Count);
+                    while (index == CurrentHealthIndex)
+                    {
+                        index = Random.Range(0, healthSpawnPoints.Count);
+                    }
+                    ExplosiveFirePickup.SpawnExplosiveFirePickup(healthSpawnPoints.ElementAt(index));
+                    CurrentFireIndex = index;
+                }
+            }
+        }
+        else
+        {
+            if (healthPickup != null)
+            {
+                if (healthPickup.IsCollected())
+                    healthPickup.SpawnHealthPickup(healthSpawnPoints.ElementAt(0));
+            }
+            else if (RapidFirePickup != null)
+            {
+                if (RapidFirePickup.IsCollected())
+                    RapidFirePickup.SpawnRapidFirePickup(healthSpawnPoints.ElementAt(0));
+            }
+            else if (ExplosiveFirePickup != null)
+            {
+                if (ExplosiveFirePickup.IsCollected())
+                    ExplosiveFirePickup.SpawnExplosiveFirePickup(healthSpawnPoints.ElementAt(0));
+            }
+        }
+    }
 
 }
 
