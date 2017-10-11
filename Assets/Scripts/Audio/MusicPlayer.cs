e03fe2c213a4b7ecad7c6b74bf2a2ad76823d679
Vivek Hari <vivek.hari@outlook.co.nz>
2017-10-11 22:19:52 +1100

    Added music controller script to control which tracks play per scene.

---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance = null;
+    public static MusicPlayer Instance
+    {
+        get { return instance; }
+    }
+
+    private AudioSource source;
+
+    private bool verified = false;
+    private AudioClip queuedClip = null;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            verified = true;
+
+            source = gameObject.AddComponent<AudioSource>();
+            if (queuedClip != null)
+            {
+                source.clip = queuedClip;
+                source.Play();
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlaySfx(AudioClip audio)
+    {
+        source.PlayOneShot(audio);
+    }
+
+    public void PlayAny(List<AudioClip> playlist, bool forceRestart)
+    {
+        var clip = playlist[Random.Range(0, playlist.Count)];
+        if (!forceRestart && clip == source.clip) return;
+
+        if (verified)
+        {
+            source.clip = clip;
+            source.Play();
+        }
+        else
+        {
+            queuedClip = clip;
+        }
+    }
+}
