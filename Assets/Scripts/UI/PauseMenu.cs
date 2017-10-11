714e517e4632f6e153c1fb8ea613d6825f5bf446
Vivek Hari <vivek.hari@outlook.co.nz>
2017-10-12 00:11:42 +1100

    Added UI sounds for buttons.

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -7,26 +7,20 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    private MusicPlayer player;
     private GameController controller;
-
-    public AudioClip buttonClickSfx;
 
     void Start()
     {
-        player = MusicPlayer.Instance;
         controller = FindObjectOfType<GameController>();
     }
 
     public void ResumeGame()
     {
         controller.ResumeGame();
-        player.PlaySfx(buttonClickSfx);
     }
 
     public void ExitGame()
     {
         controller.ExitGame();
-        player.PlaySfx(buttonClickSfx);
     }
 }
