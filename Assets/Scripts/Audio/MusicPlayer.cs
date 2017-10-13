34b88ea011c0e8149cc2e57aa3ab0d0076bd4bf8
Vivek Hari <vivek.hari@outlook.co.nz>
2017-10-13 23:48:07 +1100

    Implemented music shuffle algorithm.

--- Assets/Scripts/Audio/MusicPlayer.cs
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 using Random = UnityEngine.Random;
@@ -13,10 +14,12 @@ public class MusicPlayer : MonoBehaviour
         get { return instance; }
     }
 
-    private AudioSource source;
+    public int MaxClipsToRemember = 10;
 
+    private AudioSource source;
     private bool verified = false;
     private AudioClip queuedClip = null;
+    private List<String> memory = new List<String>();
 
     void Awake()
     {
@@ -33,9 +36,7 @@ public class MusicPlayer : MonoBehaviour
             source = gameObject.GetComponent<AudioSource>();
             if (queuedClip != null)
             {
-                source.clip = queuedClip;
-                source.loop = true;
-                source.Play();
+                PlayClip(queuedClip);
             }
         }
         DontDestroyOnLoad(this.gameObject);
@@ -48,18 +49,74 @@ public class MusicPlayer : MonoBehaviour
 
     public void PlayAny(List<AudioClip> playlist, bool forceRestart)
     {
-        var clip = playlist[Random.Range(0, playlist.Count)];
+        if (playlist == null || playlist.Count == 0) return;
+
+        AudioClip clip = playlist[0];
+        if (playlist.Count > 1)
+        {
+            // filter to least played tracks
+            var byNumTimesPlayed =
+                from track in playlist
+                group track by memory.Count(m => track.name == m) into grp
+                orderby grp.Key
+                select grp;
+            var leastPlayedTracks = byNumTimesPlayed.First();
+
+            if (leastPlayedTracks.Count() == 1)
+            {
+                // one track has been played the least, pick that one
+                clip = leastPlayedTracks.First();
+            }
+            else if (leastPlayedTracks.Key == 0)
+            {
+                // some tracks have not been played in recent memory
+                // pick a random one
+                clip = leastPlayedTracks.ElementAt(
+                    Random.Range(0, leastPlayedTracks.Count()));
+            }
+            else
+            {
+                // all tracks have been played at least once in recent memory
+                // filter to tracks that have the longest time since played
+                var byTimeSincePlayed =
+                    from track in leastPlayedTracks
+                    group track by memory.LastIndexOf(track.name) into grp
+                    orderby grp.Key
+                    select grp;
+
+                // pick a random clip
+                var longestTimeSincePlayed = byTimeSincePlayed.First();
+                clip = longestTimeSincePlayed.ElementAt(
+                    Random.Range(0, longestTimeSincePlayed.Count()));
+
+            }
+        }
+
         if (!forceRestart && clip == source.clip) return;
 
         if (verified)
         {
-            source.clip = clip;
-            source.loop = true;
-            source.Play();
+            PlayClip(clip);
         }
         else
         {
             queuedClip = clip;
+        }
+    }
+
+    private void PlayClip(AudioClip clip)
+    {
+        source.clip = clip;
+        source.loop = true;
+        source.Play();
+
+        if (MaxClipsToRemember > 0)
+        {
+            memory.Add(clip.name);
+            while (memory.Count > MaxClipsToRemember)
+            {
+                memory.RemoveAt(0);
+            }
         }
     }
 }
